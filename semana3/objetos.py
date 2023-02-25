1bb35c2b95f92b1594fadde7a83c691ec2a085ed
Marianella08 <marytore08@gmail.com>
2023-02-24 20:31:31 -0500

    nuevos

---
+++ semana3/objetos.py
@@ -0,0 +1,9 @@
+class personas():
+    sentidos = ["vista", "olfato" ,"tacto", "gusto", "oido"]
+    
+
+felipe = personas()
+gabriela = personas()
+
+print(felipe.sentidos)
+print(gabriela.sentidos)

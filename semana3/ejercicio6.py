1ae30c01ffa6b547d341ef99664a11c4c19e46f3
Marianella08 <marytore08@gmail.com>
2023-02-28 20:37:22 -0500

    8:37

---
+++ semana3/ejercicio6.py
@@ -0,0 +1,51 @@
+class SerVivo():
+    sentido_listas = ["olfato", "vista", "tacto", "gusto" "oido"]
+    
+    def __init__ (self, nombre):
+        self.nombre= nombre
+        
+    def sentidos(self):
+        print(self.sentido_listas)
+        
+    def movimiento(self):
+        print(f"{self.nombre} se esta moviendo")
+    
+    def sonido(self, X):
+        print(f"{self.nombre} esta haciendo sonido")
+        
+        
+class Persona(SerVivo):
+    def movimiento(self):
+        print(f"{self.nombre} esta caminando")
+        
+    def sonido(self, X):
+        print(f"{self.nombre} esta hablando o, tal vez, gritando")
+
+class Gato(SerVivo):
+    def movimiento(self):
+        print(f"{self.nombre} se esta merodeando")
+        
+    def sonido(self, X):
+        print(f"{self.nombre} esta maullando")
+
+class Perro(SerVivo):
+    def movimiento(self):
+        print(f"{self.nombre} se esta trotando")
+    
+    def sonido(self, X):
+        print(f"{self.nombre} esta ladrando")
+        
+P = Persona("Albino")
+P.movimiento()
+P.sonido()
+P.sentidos()
+
+g = Gato("Diablo")
+g.movimiento()
+g.sonido()
+g.sentidos()
+
+Pr = Persona("kira")
+Pr.movimiento()
+Pr.sonido()
+Pr.sentidos()
